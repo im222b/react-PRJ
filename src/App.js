db6e260b0f093bed7d61651f6fbf9159c4f2210d
im222b <rudgnsdleh@naver.com>
2023-11-08 17:42:21 +0900

    day01-1

--- src/App.js
+++ src/App.js
@@ -1,11 +1,27 @@
-import Button from "./Button";
-import styles from "./App,module.css";
+import { useState, useEffect } from "react";
 
 function App() {
+  const [counter, setValue] = useState(0);
+  const [keyword, setKeyword] = useState("");
+  const onClick = () => setValue((prev) => prev +1);
+  const onChange = (evnet) => setKeyword(evnet.target.value);
+  useEffect(() => {
+    console.log("I run only once");
+  }, []);
+  useEffect(() => {
+    console.log("I run when 'keyword' Changes.");
+  }, [keyword]);
+  useEffect(() => {
+    console.log("I run when 'counter' Changes.");
+  }, [counter]);
   return (
     <div>
-      <h1 className={styles.title}>Welcome Back!!!</h1>
-      <Button text={"Coutinue"}/>
+      <input 
+      value={keyword} 
+      onChange={onChange} 
+      type="text" placeholder="Search here....." />
+      <h1>{counter}</h1>
+      <button onClick={onClick}>Click me!</button>
     </div>
   );
 }
