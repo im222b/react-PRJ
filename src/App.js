b18851df390de9c666a6cc4bb43a3a7a0fa5b9b7
im222b <rudgnsdleh@naver.com>
2023-11-14 14:57:21 +0900

    day02

--- src/App.js
+++ src/App.js
@@ -1,28 +1,36 @@
-import { useState, useEffect } from "react";
-
-function Hello() {
-  function byFn() {
-    console.log("bye :(");
-  }
-  
-  function hiFn() {
-    console.log("crarted :)");
-    return byFn;
-  }
-    
-  useEffect((hiFn), [])
-  return <h1>Hello!</h1>;
-}
+import { useState } from "react";
 
 function App() {
-  const [showing, setshowing] = useState(false);
-  const onClick = () => setshowing((prev) => !prev);
+  const [toDo, setToDo] = useState("");
+  const [toDos, setToDos] = useState([]);
+  const onChange = (event) => setToDo(event.target.value); 
+  const onSubmit = (event) => {
+    event.preventDefault();
+    if (toDo === ""){
+      return;
+    }
+    setToDos((currentArray)=> [toDo, ...currentArray]);
+    setToDo("");
+  };
   return (
-    <div>
-      {showing ? <Hello /> : null}
-      <button onClick={onClick}>{showing ? "Hide" :"Show"}</button>
-    </div>
+  <div>
+    <h1>My To Dos ({toDos.length})</h1>
+    <form onSubmit={onSubmit}>
+    <input 
+    onChange={onChange} 
+    value={toDo} type="text" 
+    placeholder="Write your to do...."/>
+    <button>Add To Do</button>
+    </form>
+    <hr />
+    <ul>
+    {toDos.map((item,index) => ( 
+    <li key={index}>{item}</li>
+    ))}
+    </ul>
+  </div>
   );
 }
+
 
 export default App;
