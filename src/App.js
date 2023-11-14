fc3203b241c60471b69e0f302d30e242d950a2c0
im222b <rudgnsdleh@naver.com>
2023-11-14 16:16:45 +0900

    day02-1

--- src/App.js
+++ src/App.js
@@ -1,36 +1,45 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
+
 
 function App() {
-  const [toDo, setToDo] = useState("");
-  const [toDos, setToDos] = useState([]);
-  const onChange = (event) => setToDo(event.target.value); 
-  const onSubmit = (event) => {
-    event.preventDefault();
-    if (toDo === ""){
-      return;
-    }
-    setToDos((currentArray)=> [toDo, ...currentArray]);
-    setToDo("");
-  };
-  return (
+  const [loading, setLoading] = useState(true);
+  const [coins, setCoins] =useState([]);
+  const [money, setMoney] =useState(0);
+  const [result, setResult] =useState(0);
+  useEffect(() => {
+      fetch("https://api.coinpaprika.com/v1/tickers")
+      .then((response) => response.json())
+      .then((json) => {
+        setCoins(json)
+        setLoading(false);
+      });
+  }, []);
+  const inputHandle = (event) => setMoney(event.target.value);
+  const setChange = (event) => setResult(event.target.value);
+return (
   <div>
-    <h1>My To Dos ({toDos.length})</h1>
-    <form onSubmit={onSubmit}>
-    <input 
-    onChange={onChange} 
-    value={toDo} type="text" 
-    placeholder="Write your to do...."/>
-    <button>Add To Do</button>
-    </form>
-    <hr />
-    <ul>
-    {toDos.map((item,index) => ( 
-    <li key={index}>{item}</li>
-    ))}
-    </ul>
-  </div>
+    <h1>The Coins!{loading ? "" : `(${coins.length})`}</h1>
+    <input onChange={inputHandle} value={money} placeholder="Type money" />
+    <p />
+    
+    {loading ? (
+        <strong>Loading...</strong>
+      ) : (
+        <select onChange={setChange}>
+          <option value="0"> Select your coin!</option>
+          {coins.map((coin, index) => (
+            <option key={index} value={coin.quotes.USD.price}>
+              {coin.name} ({coin.symbol}): ${coin.quotes.USD.price} USD
+            </option>
+          ))}
+        </select>
+      )}
+    
+      <div>
+        <h3>You get {result == 0 ?"0" :money / result} coins</h3>  
+      </div>  
+    </div>
   );
 }
-
 
 export default App;
