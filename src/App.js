864dc39ca2cdd9eb860bba1698a090cf466fae3d
im222b <rudgnsdleh@naver.com>
2023-11-15 15:51:45 +0900

    day03

--- src/App.js
+++ src/App.js
@@ -1,35 +1,26 @@
+import Movie from "./components/movie";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+} from "react-router-dom";
+import Home from "./routes/Home";
+import Detail from "./components/Detail";
+
 const { useState, useEffect } = require("react");
 
 function App () {
-  const [loading, setLoading] =useState(true);
-  const [movies ,setMovies] = useState([]);
-  const getMovies = async () => {
-    const json = await (
-      await fetch(
-      `https://yts.mx/api/v2/list_movies.json?minimum_rating=8.9&sort_by=year`
-    )
-    ).json();
-      setMovies(json.data.movies);
-      setLoading(false);
-    };
-  useEffect(() => {
-    getMovies();
-    },[]);
-  console.log(movies);
-  return <div>
-    {loading ? <h3>Loading.... </h3> : 
-    <div>{movies.map(movie => 
-    <div key={movie.id}>
-      <img src={movie.medium_cover_image} />
-      <h3>{movie.title}</h3>
-      <ul>
-        {movie.genres.map((g) => 
-        (<li key={(g)}>{g}</li>))}
-      </ul>
-      <p>{movie.summary}</p>
-      </div>
-    )}</div>}
-  </div>;
+  return <Router>
+    <Switch>
+      <Route path="/movie">
+        <Detail />
+      </Route>
+      <Route path="/">
+      <Home />
+      </Route>
+    </Switch>
+  </Router>;
+
 }
 
 export default App;
---
+++ src/components/movie.js
@@ -0,0 +1,23 @@
+import PropTypes from "prop-types";
+
+
+function Movie({coverImg,title,summary,genres}) {
+    return <div>
+    <img src={coverImg} alt={title} />
+    <h3>{title}</h3>
+    <ul>
+        {genres.map((g) => 
+        (<li key={(g)}>{g}</li>))}
+    </ul>
+    <p>{summary}</p>
+    </div>
+}
+
+Movie.propTypes = {
+    coverImg : PropTypes.string.isRequired,
+    title : PropTypes.string.isRequired,
+    summary : PropTypes.string.isRequired,
+    genres : PropTypes.arrayOf(PropTypes.string).isRequired,
+}
+
+export default Movie;
